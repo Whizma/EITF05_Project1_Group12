95f5ef2b8ab6ec2c672ddf3897a641d6e9db92bc
ErikM <er8540ma-s@student.lu.se>
2022-09-14 12:30:47 +0200

    PHP test

---
+++ test.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+
+    <head>
+        <title>PHP test</title>
+
+    </head>
+    <body> 
+        <php>
+            echo '<p>Hello World</p>'
+
+        </php>
+    </body>
+
+</html>
