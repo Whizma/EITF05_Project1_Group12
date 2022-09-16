3c92b9c3ada02ea7135b5d092e2f4b55c68add55
ida.levison.3316 <ida.levison.3316@student.lu.se>
2022-09-16 10:46:09 +0200

    add header and footer

--- test.php
+++ test.php
@@ -1,3 +1,12 @@
+<!DOCTYPE html>
+<?php echo file_get_contents("html/header.html");?>
 <?php
 echo "Hello World";
 ?>
+
+<html> 
+    <body>
+        <h2> body section </h2>
+    </body>
+</html>
+<?php echo file_get_contents("html/footer.html");?>
