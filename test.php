fea98659c6356bbce64782275efb70097c5b99e8
ida.levison.3316 <ida.levison.3316@student.lu.se>
2022-09-16 10:51:42 +0200

    remove duplicated html tags

--- test.php
+++ test.php
@@ -1,12 +1,9 @@
-<!DOCTYPE html>
+
 <?php echo file_get_contents("html/header.html");?>
 <?php
 echo "Hello World";
 ?>
 
-<html> 
-    <body>
-        <h2> body section </h2>
-    </body>
-</html>
+<h2> body section  hej </h2>
+
 <?php echo file_get_contents("html/footer.html");?>
