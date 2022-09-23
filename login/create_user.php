93fc34b2b87ea6f35d160be8688143ed48bc42df
ErikM <er8540ma-s@student.lu.se>
2022-09-23 12:02:45 +0200

    Nu får bara unika användarnamn användas

--- login/create_user.php
+++ login/create_user.php
@@ -6,8 +6,21 @@ $username = $_GET["name"];
 $password = $_GET["password"];
 $password_hash = password_hash($password, PASSWORD_DEFAULT);
 
-$sql = "INSERT IGNORE INTO user_details (username, hash)
+
+$sqlUsernameCheck = "SELECT username
+                    FROM user_details
+                    where username = '$username'";
+
+
+if(mysqli_query($conn, $sqlUsernameCheck)){
+    echo "Username already exists";
+    CloseCon($conn);
+}
+
+
+$sql = "INSERT INTO user_details (username, hash)
         VALUES ('$username', '$password_hash')";
+
 
 if(mysqli_query($conn, $sql)) {
     echo "Welcome " . $username;
