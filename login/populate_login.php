b3aa2b89cc56e98d6bedbe21113c7260082910e7
ErikM <er8540ma-s@student.lu.se>
2022-09-26 16:11:24 +0200

    populate funkar

--- login/populate_login.php
+++ login/populate_login.php
@@ -3,20 +3,22 @@ include 'db_connection.php';
 $conn = openCon("login");
 $sqlTruncate = "TRUNCATE TABLE user_details";
 
-if(mysqli_query($sqlTruncate)) {
+if(mysqli_query($conn, $sqlTruncate)) {
     echo "Truncation successfull... ";
 } else {
     echo "Problem with truncation";
+    exit();
 }
 $passwordErik = password_hash("Malmgren", PASSWORD_DEFAULT);
 $passwordIsak = password_hash("Määttä", PASSWORD_DEFAULT);
 $passwordIda = password_hash("Levison", PASSWORD_DEFAULT);
+
 $sqlPopulate = "INSERT INTO user_details (username, hash)
                 VALUES  ('Erik', '$passwordErik'),
-                        ('Isak', '$passwordIsak')
+                        ('Isak', '$passwordIsak'),
                         ('Ida', '$passwordIda')";
 
-if(mysqli_query($sqlPopulate)){
+if(mysqli_query($conn, $sqlPopulate)){
     echo "Database populated";
 } else {
     echo "Failed to populate database";
