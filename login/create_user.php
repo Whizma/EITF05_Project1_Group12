fec470618a93fa7d42788b76fbb030666964e3de
Erik Malmgren <er8540ma-s@student.lu.se>
2022-09-26 15:08:34 +0200

    Nytt försök på create_user

--- login/create_user.php
+++ login/create_user.php
@@ -2,31 +2,33 @@
 include 'db_connection.php';
 $conn = OpenCon("login");
 
-$username = $_POST["name"];
+$username = $_POST["username"];
 $password = $_POST["password"];
 $password_hash = password_hash($password, PASSWORD_DEFAULT);
 
 
 $sqlUsernameCheck = "SELECT username
                     FROM user_details
-                    where username = '$username'";
+                    WHERE username = '$username'";
 
-// Detta blev inte så bra, querryn kommer alltid returnera true eftersom den går igenom
-if(mysqli_query($conn, $sqlUsernameCheck)){
-    echo "Username already exists";
+
+//Check if username already exists
+$usernameCheck =  mysqli_query($conn, $sqlUsernameCheck);
+if(mysqli_num_rows($usernameCheck) != 0){
+    echo "Error: Username already exists"
     CloseCon($conn);
+    exit();
 }
 
 
 $sql = "INSERT INTO user_details (username, hash)
         VALUES ('$username', '$password_hash')";
 
+mysqli_query($conn, $sql);
+echo "Welcome " . $username;
 
-if(mysqli_query($conn, $sql)) {
-    echo "Welcome " . $username;
-} else {
-    echo "Error: Username already exists";
-}
+
+//Inorder to display username on all pages, use $username = $_POST["username"]; and echo $username
 
 CloseCon($conn);
 ?>
--- login/new_user.php
+++ login/new_user.php
@@ -3,7 +3,7 @@
 <html>
 <body>
 <form action ="create_user.php" method="get">
-Username: <input type ="text" name="name"<br>
+Username: <input type ="text" name="username"<br>
 
 Password: <input type = "text" name="password"<br>
 <input type ="submit">
