1b8c2d4c380e5781b31b95c4544dc9885034fbf0
ErikM <er8540ma-s@student.lu.se>
2022-09-27 10:46:59 +0200

    Nu kan man logga in men inte ut

---
+++ login/login.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+
+<html>
+    <body>
+        <form action ="check_login.php" method="post">
+            Username: <input type = "text" name="new_name"><br>
+            Password: <input type = "text" name="new_password"><br>
+            <input type ="submit">
+        </form>
+    </body>
+</html>
+
+
