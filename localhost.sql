07a76875b3a199b43e0711988a5d2a166e6f492b
ErikM <er8540ma-s@student.lu.se>
2022-09-30 10:12:14 +0200

    DB också

--- localhost.sql
+++ localhost.sql
@@ -2,10 +2,10 @@
 -- version 5.2.0
 -- https://www.phpmyadmin.net/
 --
--- Värd: localhost
--- Tid vid skapande: 30 sep 2022 kl 10:02
--- Serverversion: 10.4.21-MariaDB
--- PHP-version: 8.1.6
+-- Host: localhost
+-- Generation Time: Sep 30, 2022 at 10:11 AM
+-- Server version: 10.4.24-MariaDB
+-- PHP Version: 8.1.6
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Databas: `login`
+-- Database: `login`
 --
 DROP DATABASE IF EXISTS `login`;
 CREATE DATABASE IF NOT EXISTS `login` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
@@ -27,7 +27,24 @@ USE `login`;
 -- --------------------------------------------------------
 
 --
--- Tabellstruktur `user_details`
+-- Table structure for table `blacklisted`
+--
+
+CREATE TABLE `blacklisted` (
+  `bl_pwd` varchar(60) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `blacklisted`
+--
+
+INSERT INTO `blacklisted` (`bl_pwd`) VALUES
+('123456');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_details`
 --
 
 CREATE TABLE `user_details` (
@@ -36,7 +53,7 @@ CREATE TABLE `user_details` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumpning av Data i tabell `user_details`
+-- Dumping data for table `user_details`
 --
 
 INSERT INTO `user_details` (`username`, `hash`) VALUES
@@ -45,16 +62,16 @@ INSERT INTO `user_details` (`username`, `hash`) VALUES
 ('Isak', '$2y$10$Calo.bhfzrgBpBGXKvuS5eAF1woTlBLbEbHSkgiRpBLsD6lx6DI3S');
 
 --
--- Index för dumpade tabeller
+-- Indexes for dumped tables
 --
 
 --
--- Index för tabell `user_details`
+-- Indexes for table `user_details`
 --
 ALTER TABLE `user_details`
   ADD PRIMARY KEY (`username`);
 --
--- Databas: `phpmyadmin`
+-- Database: `phpmyadmin`
 --
 DROP DATABASE IF EXISTS `phpmyadmin`;
 CREATE DATABASE IF NOT EXISTS `phpmyadmin` DEFAULT CHARACTER SET utf8 COLLATE utf8_bin;
@@ -63,7 +80,7 @@ USE `phpmyadmin`;
 -- --------------------------------------------------------
 
 --
--- Tabellstruktur `pma__bookmark`
+-- Table structure for table `pma__bookmark`
 --
 
 CREATE TABLE `pma__bookmark` (
@@ -77,7 +94,7 @@ CREATE TABLE `pma__bookmark` (
 -- --------------------------------------------------------
 
 --
--- Tabellstruktur `pma__central_columns`
+-- Table structure for table `pma__central_columns`
 --
 
 CREATE TABLE `pma__central_columns` (
@@ -94,7 +111,7 @@ CREATE TABLE `pma__central_columns` (
 -- --------------------------------------------------------
 
 --
--- Tabellstruktur `pma__column_info`
+-- Table structure for table `pma__column_info`
 --
 
 CREATE TABLE `pma__column_info` (
@@ -113,7 +130,7 @@ CREATE TABLE `pma__column_info` (
 -- --------------------------------------------------------
 
 --
--- Tabellstruktur `pma__designer_settings`
+-- Table structure for table `pma__designer_settings`
 --
 
 CREATE TABLE `pma__designer_settings` (
@@ -124,7 +141,7 @@ CREATE TABLE `pma__designer_settings` (
 -- --------------------------------------------------------
 
 --
--- Tabellstruktur `pma__export_templates`
+-- Table structure for table `pma__export_templates`
 --
 
 CREATE TABLE `pma__export_templates` (
@@ -135,10 +152,17 @@ CREATE TABLE `pma__export_templates` (
   `template_data` text COLLATE utf8_bin NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin COMMENT='Saved export templates';
 
+--
+-- Dumping data for table `pma__export_templates`
+--
+
+INSERT INTO `pma__export_templates` (`id`, `username`, `export_type`, `template_name`, `template_data`) VALUES
+(1, 'root', 'server', 'Replace DB', '{\"quick_or_custom\":\"custom\",\"what\":\"sql\",\"db_select[]\":[\"login\",\"phpmyadmin\",\"products\",\"test\",\"testDB\"],\"aliases_new\":\"\",\"output_format\":\"sendit\",\"filename_template\":\"@SERVER@\",\"remember_template\":\"on\",\"charset\":\"utf-8\",\"compression\":\"none\",\"maxsize\":\"\",\"codegen_structure_or_data\":\"data\",\"codegen_format\":\"0\",\"csv_separator\":\",\",\"csv_enclosed\":\"\\\"\",\"csv_escaped\":\"\\\"\",\"csv_terminated\":\"AUTO\",\"csv_null\":\"NULL\",\"csv_structure_or_data\":\"data\",\"excel_null\":\"NULL\",\"excel_columns\":\"something\",\"excel_edition\":\"win\",\"excel_structure_or_data\":\"data\",\"json_structure_or_data\":\"data\",\"json_unicode\":\"something\",\"latex_caption\":\"something\",\"latex_structure_or_data\":\"structure_and_data\",\"latex_structure_caption\":\"Structure of table @TABLE@\",\"latex_structure_continued_caption\":\"Structure of table @TABLE@ (continued)\",\"latex_structure_label\":\"tab:@TABLE@-structure\",\"latex_relation\":\"something\",\"latex_comments\":\"something\",\"latex_mime\":\"something\",\"latex_columns\":\"something\",\"latex_data_caption\":\"Content of table @TABLE@\",\"latex_data_continued_caption\":\"Content of table @TABLE@ (continued)\",\"latex_data_label\":\"tab:@TABLE@-data\",\"latex_null\":\"\\\\textit{NULL}\",\"mediawiki_structure_or_data\":\"data\",\"mediawiki_caption\":\"something\",\"mediawiki_headers\":\"something\",\"htmlword_structure_or_data\":\"structure_and_data\",\"htmlword_null\":\"NULL\",\"ods_null\":\"NULL\",\"ods_structure_or_data\":\"data\",\"odt_structure_or_data\":\"structure_and_data\",\"odt_relation\":\"something\",\"odt_comments\":\"something\",\"odt_mime\":\"something\",\"odt_columns\":\"something\",\"odt_null\":\"NULL\",\"pdf_report_title\":\"\",\"pdf_structure_or_data\":\"data\",\"phparray_structure_or_data\":\"data\",\"sql_include_comments\":\"something\",\"sql_header_comment\":\"\",\"sql_use_transaction\":\"something\",\"sql_compatibility\":\"NONE\",\"sql_structure_or_data\":\"structure_and_data\",\"sql_drop_database\":\"something\",\"sql_create_table\":\"something\",\"sql_auto_increment\":\"something\",\"sql_create_view\":\"something\",\"sql_create_trigger\":\"something\",\"sql_backquotes\":\"something\",\"sql_type\":\"INSERT\",\"sql_insert_syntax\":\"both\",\"sql_max_query_size\":\"50000\",\"sql_hex_for_binary\":\"something\",\"sql_utc_time\":\"something\",\"texytext_structure_or_data\":\"structure_and_data\",\"texytext_null\":\"NULL\",\"yaml_structure_or_data\":\"data\",\"\":null,\"as_separate_files\":null,\"csv_removeCRLF\":null,\"csv_columns\":null,\"excel_removeCRLF\":null,\"json_pretty_print\":null,\"htmlword_columns\":null,\"ods_columns\":null,\"sql_dates\":null,\"sql_relation\":null,\"sql_mime\":null,\"sql_disable_fk\":null,\"sql_views_as_tables\":null,\"sql_metadata\":null,\"sql_drop_table\":null,\"sql_if_not_exists\":null,\"sql_simple_view_export\":null,\"sql_view_current_user\":null,\"sql_or_replace_view\":null,\"sql_procedure_function\":null,\"sql_truncate\":null,\"sql_delayed\":null,\"sql_ignore\":null,\"texytext_columns\":null}');
+
 -- --------------------------------------------------------
 
 --
--- Tabellstruktur `pma__favorite`
+-- Table structure for table `pma__favorite`
 --
 
 CREATE TABLE `pma__favorite` (
@@ -149,7 +173,7 @@ CREATE TABLE `pma__favorite` (
 -- --------------------------------------------------------
 
 --
--- Tabellstruktur `pma__history`
+-- Table structure for table `pma__history`
 --
 
 CREATE TABLE `pma__history` (
@@ -164,7 +188,7 @@ CREATE TABLE `pma__history` (
 -- --------------------------------------------------------
 
 --
--- Tabellstruktur `pma__navigationhiding`
+-- Table structure for table `pma__navigationhiding`
 --
 
 CREATE TABLE `pma__navigationhiding` (
@@ -178,7 +202,7 @@ CREATE TABLE `pma__navigationhiding` (
 -- --------------------------------------------------------
 
 --
--- Tabellstruktur `pma__pdf_pages`
+-- Table structure for table `pma__pdf_pages`
 --
 
 CREATE TABLE `pma__pdf_pages` (
@@ -190,7 +214,7 @@ CREATE TABLE `pma__pdf_pages` (
 -- --------------------------------------------------------
 
 --
--- Tabellstruktur `pma__recent`
+-- Table structure for table `pma__recent`
 --
 
 CREATE TABLE `pma__recent` (
@@ -199,16 +223,16 @@ CREATE TABLE `pma__recent` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin COMMENT='Recently accessed tables';
 
 --
--- Dumpning av Data i tabell `pma__recent`
+-- Dumping data for table `pma__recent`
 --
 
 INSERT INTO `pma__recent` (`username`, `tables`) VALUES
-('root', '[{\"db\":\"products\",\"table\":\"product_table\"},{\"db\":\"login\",\"table\":\"user_details\"}]');
+('root', '[{\"db\":\"login\",\"table\":\"blacklisted\"},{\"db\":\"products\",\"table\":\"product_table\"},{\"db\":\"login\",\"table\":\"user_details\"}]');
 
 -- --------------------------------------------------------
 
 --
--- Tabellstruktur `pma__relation`
+-- Table structure for table `pma__relation`
 --
 
 CREATE TABLE `pma__relation` (
@@ -223,7 +247,7 @@ CREATE TABLE `pma__relation` (
 -- --------------------------------------------------------
 
 --
--- Tabellstruktur `pma__savedsearches`
+-- Table structure for table `pma__savedsearches`
 --
 
 CREATE TABLE `pma__savedsearches` (
@@ -237,7 +261,7 @@ CREATE TABLE `pma__savedsearches` (
 -- --------------------------------------------------------
 
 --
--- Tabellstruktur `pma__table_coords`
+-- Table structure for table `pma__table_coords`
 --
 
 CREATE TABLE `pma__table_coords` (
@@ -251,7 +275,7 @@ CREATE TABLE `pma__table_coords` (
 -- --------------------------------------------------------
 
 --
--- Tabellstruktur `pma__table_info`
+-- Table structure for table `pma__table_info`
 --
 
 CREATE TABLE `pma__table_info` (
@@ -263,7 +287,7 @@ CREATE TABLE `pma__table_info` (
 -- --------------------------------------------------------
 
 --
--- Tabellstruktur `pma__table_uiprefs`
+-- Table structure for table `pma__table_uiprefs`
 --
 
 CREATE TABLE `pma__table_uiprefs` (
@@ -277,7 +301,7 @@ CREATE TABLE `pma__table_uiprefs` (
 -- --------------------------------------------------------
 
 --
--- Tabellstruktur `pma__tracking`
+-- Table structure for table `pma__tracking`
 --
 
 CREATE TABLE `pma__tracking` (
@@ -296,7 +320,7 @@ CREATE TABLE `pma__tracking` (
 -- --------------------------------------------------------
 
 --
--- Tabellstruktur `pma__userconfig`
+-- Table structure for table `pma__userconfig`
 --
 
 CREATE TABLE `pma__userconfig` (
@@ -306,16 +330,16 @@ CREATE TABLE `pma__userconfig` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin COMMENT='User preferences storage for phpMyAdmin';
 
 --
--- Dumpning av Data i tabell `pma__userconfig`
+-- Dumping data for table `pma__userconfig`
 --
 
 INSERT INTO `pma__userconfig` (`username`, `timevalue`, `config_data`) VALUES
-('root', '2022-09-30 07:51:29', '{\"lang\":\"sv\",\"Console\\/Mode\":\"collapse\"}');
+('root', '2022-09-30 08:04:06', '{\"Console\\/Mode\":\"collapse\"}');
 
 -- --------------------------------------------------------
 
 --
--- Tabellstruktur `pma__usergroups`
+-- Table structure for table `pma__usergroups`
 --
 
 CREATE TABLE `pma__usergroups` (
@@ -327,7 +351,7 @@ CREATE TABLE `pma__usergroups` (
 -- --------------------------------------------------------
 
 --
--- Tabellstruktur `pma__users`
+-- Table structure for table `pma__users`
 --
 
 CREATE TABLE `pma__users` (
@@ -336,170 +360,170 @@ CREATE TABLE `pma__users` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin COMMENT='Users and their assignments to user groups';
 
 --
--- Index för dumpade tabeller
+-- Indexes for dumped tables
 --
 
 --
--- Index för tabell `pma__bookmark`
+-- Indexes for table `pma__bookmark`
 --
 ALTER TABLE `pma__bookmark`
   ADD PRIMARY KEY (`id`);
 
 --
--- Index för tabell `pma__central_columns`
+-- Indexes for table `pma__central_columns`
 --
 ALTER TABLE `pma__central_columns`
   ADD PRIMARY KEY (`db_name`,`col_name`);
 
 --
--- Index för tabell `pma__column_info`
+-- Indexes for table `pma__column_info`
 --
 ALTER TABLE `pma__column_info`
   ADD PRIMARY KEY (`id`),
   ADD UNIQUE KEY `db_name` (`db_name`,`table_name`,`column_name`);
 
 --
--- Index för tabell `pma__designer_settings`
+-- Indexes for table `pma__designer_settings`
 --
 ALTER TABLE `pma__designer_settings`
   ADD PRIMARY KEY (`username`);
 
 --
--- Index för tabell `pma__export_templates`
+-- Indexes for table `pma__export_templates`
 --
 ALTER TABLE `pma__export_templates`
   ADD PRIMARY KEY (`id`),
   ADD UNIQUE KEY `u_user_type_template` (`username`,`export_type`,`template_name`);
 
 --
--- Index för tabell `pma__favorite`
+-- Indexes for table `pma__favorite`
 --
 ALTER TABLE `pma__favorite`
   ADD PRIMARY KEY (`username`);
 
 --
--- Index för tabell `pma__history`
+-- Indexes for table `pma__history`
 --
 ALTER TABLE `pma__history`
   ADD PRIMARY KEY (`id`),
   ADD KEY `username` (`username`,`db`,`table`,`timevalue`);
 
 --
--- Index för tabell `pma__navigationhiding`
+-- Indexes for table `pma__navigationhiding`
 --
 ALTER TABLE `pma__navigationhiding`
   ADD PRIMARY KEY (`username`,`item_name`,`item_type`,`db_name`,`table_name`);
 
 --
--- Index för tabell `pma__pdf_pages`
+-- Indexes for table `pma__pdf_pages`
 --
 ALTER TABLE `pma__pdf_pages`
   ADD PRIMARY KEY (`page_nr`),
   ADD KEY `db_name` (`db_name`);
 
 --
--- Index för tabell `pma__recent`
+-- Indexes for table `pma__recent`
 --
 ALTER TABLE `pma__recent`
   ADD PRIMARY KEY (`username`);
 
 --
--- Index för tabell `pma__relation`
+-- Indexes for table `pma__relation`
 --
 ALTER TABLE `pma__relation`
   ADD PRIMARY KEY (`master_db`,`master_table`,`master_field`),
   ADD KEY `foreign_field` (`foreign_db`,`foreign_table`);
 
 --
--- Index för tabell `pma__savedsearches`
+-- Indexes for table `pma__savedsearches`
 --
 ALTER TABLE `pma__savedsearches`
   ADD PRIMARY KEY (`id`),
   ADD UNIQUE KEY `u_savedsearches_username_dbname` (`username`,`db_name`,`search_name`);
 
 --
--- Index för tabell `pma__table_coords`
+-- Indexes for table `pma__table_coords`
 --
 ALTER TABLE `pma__table_coords`
   ADD PRIMARY KEY (`db_name`,`table_name`,`pdf_page_number`);
 
 --
--- Index för tabell `pma__table_info`
+-- Indexes for table `pma__table_info`
 --
 ALTER TABLE `pma__table_info`
   ADD PRIMARY KEY (`db_name`,`table_name`);
 
 --
--- Index för tabell `pma__table_uiprefs`
+-- Indexes for table `pma__table_uiprefs`
 --
 ALTER TABLE `pma__table_uiprefs`
   ADD PRIMARY KEY (`username`,`db_name`,`table_name`);
 
 --
--- Index för tabell `pma__tracking`
+-- Indexes for table `pma__tracking`
 --
 ALTER TABLE `pma__tracking`
   ADD PRIMARY KEY (`db_name`,`table_name`,`version`);
 
 --
--- Index för tabell `pma__userconfig`
+-- Indexes for table `pma__userconfig`
 --
 ALTER TABLE `pma__userconfig`
   ADD PRIMARY KEY (`username`);
 
 --
--- Index för tabell `pma__usergroups`
+-- Indexes for table `pma__usergroups`
 --
 ALTER TABLE `pma__usergroups`
   ADD PRIMARY KEY (`usergroup`,`tab`,`allowed`);
 
 --
--- Index för tabell `pma__users`
+-- Indexes for table `pma__users`
 --
 ALTER TABLE `pma__users`
   ADD PRIMARY KEY (`username`,`usergroup`);
 
 --
--- AUTO_INCREMENT för dumpade tabeller
+-- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT för tabell `pma__bookmark`
+-- AUTO_INCREMENT for table `pma__bookmark`
 --
 ALTER TABLE `pma__bookmark`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT för tabell `pma__column_info`
+-- AUTO_INCREMENT for table `pma__column_info`
 --
 ALTER TABLE `pma__column_info`
   MODIFY `id` int(5) UNSIGNED NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT för tabell `pma__export_templates`
+-- AUTO_INCREMENT for table `pma__export_templates`
 --
 ALTER TABLE `pma__export_templates`
-  MODIFY `id` int(5) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(5) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
--- AUTO_INCREMENT för tabell `pma__history`
+-- AUTO_INCREMENT for table `pma__history`
 --
 ALTER TABLE `pma__history`
   MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT för tabell `pma__pdf_pages`
+-- AUTO_INCREMENT for table `pma__pdf_pages`
 --
 ALTER TABLE `pma__pdf_pages`
   MODIFY `page_nr` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT för tabell `pma__savedsearches`
+-- AUTO_INCREMENT for table `pma__savedsearches`
 --
 ALTER TABLE `pma__savedsearches`
   MODIFY `id` int(5) UNSIGNED NOT NULL AUTO_INCREMENT;
 --
--- Databas: `products`
+-- Database: `products`
 --
 DROP DATABASE IF EXISTS `products`;
 CREATE DATABASE IF NOT EXISTS `products` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
@@ -508,7 +532,7 @@ USE `products`;
 -- --------------------------------------------------------
 
 --
--- Tabellstruktur `product_table`
+-- Table structure for table `product_table`
 --
 
 CREATE TABLE `product_table` (
@@ -519,7 +543,7 @@ CREATE TABLE `product_table` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumpning av Data i tabell `product_table`
+-- Dumping data for table `product_table`
 --
 
 INSERT INTO `product_table` (`id`, `name`, `price`, `img`) VALUES
@@ -527,20 +551,49 @@ INSERT INTO `product_table` (`id`, `name`, `price`, `img`) VALUES
 (2, 'transparent', '14.50', 'trans.jpeg');
 
 --
--- Index för dumpade tabeller
+-- Indexes for dumped tables
 --
 
 --
--- Index för tabell `product_table`
+-- Indexes for table `product_table`
 --
 ALTER TABLE `product_table`
   ADD PRIMARY KEY (`id`);
 --
--- Databas: `test`
+-- Database: `test`
 --
 DROP DATABASE IF EXISTS `test`;
 CREATE DATABASE IF NOT EXISTS `test` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
 USE `test`;
+--
+-- Database: `testDB`
+--
+DROP DATABASE IF EXISTS `testDB`;
+CREATE DATABASE IF NOT EXISTS `testDB` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+USE `testDB`;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `testUser`
+--
+
+CREATE TABLE `testUser` (
+  `Username` text NOT NULL,
+  `Hash` text NOT NULL,
+  `Salt` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `testUser`
+--
+
+INSERT INTO `testUser` (`Username`, `Hash`, `Salt`) VALUES
+('Isak', '1234', '56'),
+('Isak', '1234', '56'),
+('Isak', '1234', '56'),
+('Isak', '1234', '56'),
+('Isak', '1234', '56');
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
