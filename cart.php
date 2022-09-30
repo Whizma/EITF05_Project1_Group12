622d65de11c8566bbba98c441db3100c90b1e737
ida.levison.3316 <ida.levison.3316@student.lu.se>
2022-09-30 12:03:57 +0200

    wip add to cart

--- cart.php
+++ cart.php
@@ -1,16 +1,17 @@
 <?php
 // If the user clicked the add to cart button on the product page we can check for the form data
 if (isset($_POST['product_id'], $_POST['quantity']) && is_numeric($_POST['product_id']) && is_numeric($_POST['quantity'])) {
-    // Set the post variables so we easily identify them, also make sure they are integer
+    //Set the post variables so we easily identify them, also make sure they are integer
     $product_id = (int)$_POST['product_id'];
     $quantity = (int)$_POST['quantity'];
     // Prepare the SQL statement, we basically are checking if the product exists in our databaser
-    $stmt = $conn->prepare('SELECT * FROM products WHERE id = ?');
-    $stmt->bind_param("s", $product_id);
+    $stmt = "SELECT * FROM product_table WHERE id = $product_id"; //OBS not safe
+    // $stmt->bind_param("s", $product_id);
     $result = mysqli_query($conn, $stmt);
-    // Fetch the product from the database and return the result as an Array
+    //Fetch the product from the database and return the result as an Array
     $product = $result->fetch_all(MYSQLI_ASSOC);
-    // Check if the product exists (array is not empty)
+    //Check if the product exists (array is not empty)
+    echo $product['name'];
     if ($product && $quantity > 0) {
         // Product exists in database, now we can create/update the session variable for the cart
         if (isset($_SESSION['cart']) && is_array($_SESSION['cart'])) {
@@ -28,60 +29,60 @@ if (isset($_POST['product_id'], $_POST['quantity']) && is_numeric($_POST['produc
     }
     // Prevent form resubmission...
     header('location: index1.php?page=cart');
-    exit;
+    // exit;
 }
 
-// Remove product from cart, check for the URL param "remove", this is the product id, make sure it's a number and check if it's in the cart
-if (isset($_GET['remove']) && is_numeric($_GET['remove']) && isset($_SESSION['cart']) && isset($_SESSION['cart'][$_GET['remove']])) {
-    // Remove the product from the shopping cart
-    unset($_SESSION['cart'][$_GET['remove']]);
-}
+// // Remove product from cart, check for the URL param "remove", this is the product id, make sure it's a number and check if it's in the cart
+// if (isset($_GET['remove']) && is_numeric($_GET['remove']) && isset($_SESSION['cart']) && isset($_SESSION['cart'][$_GET['remove']])) {
+//     // Remove the product from the shopping cart
+//     unset($_SESSION['cart'][$_GET['remove']]);
+// }
 
-// Update product quantities in cart if the user clicks the "Update" button on the shopping cart page
-if (isset($_POST['update']) && isset($_SESSION['cart'])) {
-    // Loop through the post data so we can update the quantities for every product in cart
-    foreach ($_POST as $k => $v) {
-        if (strpos($k, 'quantity') !== false && is_numeric($v)) {
-            $id = str_replace('quantity-', '', $k);
-            $quantity = (int)$v;
-            // Always do checks and validation
-            if (is_numeric($id) && isset($_SESSION['cart'][$id]) && $quantity > 0) {
-                // Update new quantity
-                $_SESSION['cart'][$id] = $quantity;
-            }
-        }
-    }
-    // Prevent form resubmission...
-    header('location: index1.php?page=cart');
-    exit;
-}
+// // Update product quantities in cart if the user clicks the "Update" button on the shopping cart page
+// if (isset($_POST['update']) && isset($_SESSION['cart'])) {
+//     // Loop through the post data so we can update the quantities for every product in cart
+//     foreach ($_POST as $k => $v) {
+//         if (strpos($k, 'quantity') !== false && is_numeric($v)) {
+//             $id = str_replace('quantity-', '', $k);
+//             $quantity = (int)$v;
+//             // Always do checks and validation
+//             if (is_numeric($id) && isset($_SESSION['cart'][$id]) && $quantity > 0) {
+//                 // Update new quantity
+//                 $_SESSION['cart'][$id] = $quantity;
+//             }
+//         }
+//     }
+//     // Prevent form resubmission...
+//     header('location: index1.php?page=cart');
+//     exit;
+// }
 
-// Send the user to the place order page if they click the Place Order button, also the cart should not be empty
-if (isset($_POST['placeorder']) && isset($_SESSION['cart']) && !empty($_SESSION['cart'])) {
-    header('Location: index1.php?page=placeorder');
-    exit;
-}
+// // Send the user to the place order page if they click the Place Order button, also the cart should not be empty
+// if (isset($_POST['placeorder']) && isset($_SESSION['cart']) && !empty($_SESSION['cart'])) {
+//     header('Location: index1.php?page=placeorder');
+//     exit;
+// }
 
 // Check the session variable for products in cart
-$products_in_cart = isset($_SESSION['cart']) ? $_SESSION['cart'] : array();
-$products = array();
-$subtotal = 0.00;
-// If there are products in cart
-if ($products_in_cart) {
-    // There are products in the cart so we need to select those products from the database
-    // Products in cart array to question mark string array, we need the SQL statement to include IN (?,?,?,...etc)
-    $array_to_question_marks = implode(',', array_fill(0, count($products_in_cart), '?'));
-    $stmt = $conn->prepare('SELECT * FROM products WHERE id IN ?');
-    $stmt->bind_param("s", $array_to_question_marks);
-    // We only need the array keys, not the values, the keys are the id's of the products
-    $stmt = mysqli_query($conn, $stmt);
-    // Fetch the product from the database and return the result as an Array
-    $product = $stmt->fetch_all(MYSQLI_ASSOC);
-    // Calculate the subtotal
-    foreach ($products as $product) {
-        $subtotal += (float)$product['price'] * (int)$products_in_cart[$product['id']];
-    }
-}
+// $products_in_cart = isset($_SESSION['cart']) ? $_SESSION['cart'] : array();
+// $products = array();
+// $subtotal = 0.00;
+// // If there are products in cart
+// if ($products_in_cart) {
+//     // There are products in the cart so we need to select those products from the database
+//     // Products in cart array to question mark string array, we need the SQL statement to include IN (?,?,?,...etc)
+//     $array_to_question_marks = implode(',', array_fill(0, count($products_in_cart), '?'));
+//     $stmt = $conn->prepare('SELECT * FROM product_table WHERE id IN ?');
+//     $stmt->bind_param("s", $array_to_question_marks);
+//     // We only need the array keys, not the values, the keys are the id's of the products
+//     $stmt = mysqli_query($conn, $stmt);
+//     // Fetch the product from the database and return the result as an Array
+//     $product = $stmt->fetch_all(MYSQLI_ASSOC);
+//     // Calculate the subtotal
+//     foreach ($products as $product) {
+//         $subtotal += (float)$product['price'] * (int)$products_in_cart[$product['id']];
+//     }
+// }
 ?>
 
 
