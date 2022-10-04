cc5d7e36e6ee473e327a63000b29db0e5db9724d
ErikM <er8540ma-s@student.lu.se>
2022-10-04 11:18:00 +0200

    logout v0

--- login/db_connection.php
+++ login/db_connection.php
@@ -26,6 +26,17 @@ function welcomeUser()
 
 function logout() 
 {
+    session_start();
+    //$_SESSION["name"] ="";
+    $_SESSION = array();
+    if(ini_get("session.use_cookies")){
+        $params = session_get_cookie_params();
+        setcookie(session_name(), '', time() - 42000,
+            $params["path"], $params["domain"],
+            $params["secure"], $params["httponly"]
+        );
+    }
+    session_destroy();
 }
 
 ?>
