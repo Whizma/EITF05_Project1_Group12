43af69728cad3f2ab36e4177c8f2b2f0fca48195
isak <isak@maatta.se>
2022-10-10 11:32:31 +0200

    praktiska biten av csrfskydd verkar fungera

--- login/delete_user.php
+++ login/delete_user.php
@@ -1,12 +1,33 @@
-<!DOCTYPE html>
+<?php 
 
+session_start();
+if (empty($_SESSION['token'])) {
+  $_SESSION['token'] = bin2hex(random_bytes(32));
+}
+$token = $_SESSION['token'];
+
+
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+
+    if (!hash_equals($_POST["token"], $token)) {
+        // proceed
+        header($_SERVER["SERVER_PROTOCOL"]." 418 I'm a teapot", true, 405);
+    }
+    else {
+        // döda skiten
+        header($_SERVER["SERVER_PROTOCOL"]." 405 Method Not Allowed", true, 405);
+        exit;
+    }
+}
+
+?>
+<!DOCTYPE html>
 <html>
     <body>
-        
-    <form action="process_delete_user.php" method ="post">
-        Token: <input type= "text" name="token"><br>
+    <form action="process_delete_user.php" method ="POST">
+        Token: <input type= "text" name="token"> <br>
         <input type ="submit">
+        <input type ="hidden" name = "token" value = $token
     </form>
     </body>
 </html>
-
--- login/process_delete_user.php
+++ login/process_delete_user.php
@@ -3,5 +3,21 @@
 session_start();
 include 'db_connection.php';
 $conn = openCon("login");
-echo "user deleted";
+if (empty($_SESSION['token'])) {
+    $_SESSION['token'] = bin2hex(random_bytes(32));
+  }
+  $token = $_SESSION['token'];
+  
+  if($_SERVER["REQUEST_METHOD"] == "POST"){
+  
+      if (hash_equals($_POST["token"], $token)) {
+          // proceed
+          header($_SERVER["SERVER_PROTOCOL"]." 418 I'm a teapot", true, 405);
+      }
+      else {
+          // döda skiten
+          header($_SERVER["SERVER_PROTOCOL"]." 405 Method Not Allowed", true, 405);
+          exit;
+      }
+  }
 ?>
